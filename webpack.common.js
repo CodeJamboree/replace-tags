7217df268d7bb03c483caee359a6103173993989
Lewis Moten <lewis.moten@codejamboree.com>
2024-03-24 00:06:41 -0400

    setup webpack

---
+++ webpack.common.js
@@ -0,0 +1,40 @@
+/* eslint-env node */
+const path = require("path");
+const packageJson = require("./package.json");
+
+module.exports = (env, argv) => {
+  // load up production values
+
+  const isProduction = argv.mode === 'production';
+
+  const dist = `./dist${isProduction ? '' : '-dev'}`;
+  const jsExtension = isProduction ? '.min.js' : '.js';
+  const unscopedName = packageJson.name.split('/').pop();
+  const config = {
+    entry: "./src/index.ts",
+    output: {
+      clean: true,
+      asyncChunks: true,
+      library: packageJson.name,
+      libraryTarget: 'umd',
+      path: path.resolve(__dirname, dist),
+      filename: `${unscopedName}${jsExtension}`,
+      sourceMapFilename: "[file].map",
+      chunkFilename: `[name]${jsExtension}`,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          use: "source-map-loader",
+          enforce: "pre",
+          exclude: /node_modules/,
+        },
+      ],
+    },
+    resolve: {
+      extensions: [".ts", ".js", ".json"],
+    },
+  };
+  return config;
+};
---
+++ webpack.prod.js
@@ -0,0 +1,35 @@
+/* eslint-env node */
+const common = require("./webpack.common");
+const TerserPlugin = require("terser-webpack-plugin");
+
+module.exports = (env, argv) => {
+  let config = common(env, argv);
+
+  return {
+    ...config,
+    mode: "production",
+    devtool: "source-map",
+    optimization: {
+      minimize: true,
+      minimizer: [new TerserPlugin()]
+    },
+    module: {
+      ...config.module,
+      rules: [
+        ...config.module.rules,
+        {
+          test: /\.jsx?$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              cacheDirectory: true,
+              cacheCompression: false,
+              envName: "production",
+            },
+          },
+        },
+      ],
+    },
+  };
+};
