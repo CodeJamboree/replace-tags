41178bb1b7390b10d64bd44218d6604001ec3db4
Lewis Moten <lewis.moten@codejamboree.com>
2024-03-24 13:49:53 -0400

    format

--- src/__tests__/dist.test.ts
+++ src/__tests__/dist.test.ts
@@ -1,10 +1,9 @@
 import replaceTags from "../../dist/replace-tags.min.js";
 
-describe('dist', () => {
+describe("dist", () => {
   it("replaces tag", () => {
     const text = "Hello {{name}}!";
     const values = { name: "World" };
     expect(replaceTags(text, values)).toBe("Hello World!");
   });
-
 });
