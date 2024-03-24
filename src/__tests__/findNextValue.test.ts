596def63af4863e302331447d8d3a7ec92f340f1
Lewis Moten <lewis.moten@codejamboree.com>
2024-03-24 10:01:51 -0400

    remove comments

--- src/__tests__/findNextValue.test.ts
+++ src/__tests__/findNextValue.test.ts
@@ -25,22 +25,3 @@ describe("findNextValue", () => {
     });
   });
 });
-/*
-
-const findNextValue = (result: NextValue, segment: string): NextValue => {
-  let {value, currentPath, fullPath} = result;
-  if(value === undefined) return result;
-  if (arrayPattern.test(segment)) {
-    value = getArrayValue(value, segment, currentPath, fullPath);
-    currentPath = appendPath(currentPath, segment);
-  } else {
-    currentPath = appendPath(currentPath, segment);
-    value = getValue(value, segment, currentPath, fullPath);
-  }
-  return {
-    value,
-    currentPath,
-    fullPath
-  }
-}
-*/
