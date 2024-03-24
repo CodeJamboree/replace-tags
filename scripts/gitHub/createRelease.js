582c0786ab3a54d64acaf02123f0ac1e3bf09a52
Lewis Moten <lewis.moten@codejamboree.com>
2024-03-24 11:32:23 -0400

    handle errors better

--- scripts/gitHub/createRelease.js
+++ scripts/gitHub/createRelease.js
@@ -6,11 +6,16 @@ createRelease = (
   title = `Release ${tag}`,
   notes = "This is an automated release.",
 ) => {
-  // brew install gh
-  execSync(
-    `gh release create "${tag}" "${dir}" --title "${title}" --notes "${notes}" --draft=false --prerelease=false`,
-  );
-  console.log("released on github.");
+  try {
+    // brew install gh
+    execSync(
+      `gh release create "${tag}" "${dir}" --title "${title}" --notes "${notes}" --draft=false --prerelease=false`,
+    );
+    console.log("released on github.");
+  } catch (error) {
+    console.error("Failed releasing to github:", error.message);
+    process.exit(1);
+  }
 };
 
 module.exports = createRelease;
