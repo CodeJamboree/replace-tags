6bd9423fcd6364293eb8465f4dbbc668d9fe6af7
Lewis Moten <lewismoten@gmail.com>
2024-03-25 18:56:33 -0400

    :white_check_mark:fix tags

--- src/styles/__tests__/SquareBracketsWithHyphens.test.ts
+++ src/styles/__tests__/SquareBracketsWithHyphens.test.ts
@@ -2,5 +2,5 @@ import SquareBracketsWithHyphens from "../SquareBracketsWithHyphens";
 import runTagStyleTests from "./runTagStyleTests";
 
 describe("SquareBracketsWithHyphens", () => {
-  runTagStyleTests("[#", "#]", SquareBracketsWithHyphens);
+  runTagStyleTests("[-", "-]", SquareBracketsWithHyphens);
 });
