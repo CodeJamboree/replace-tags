6d5b49998947e19b18187116656104af059b34fe
Lewis Moten <lewismoten@gmail.com>
2024-03-26 16:37:21 -0400

    :art: Pretty

--- src/globals.d.ts
+++ src/globals.d.ts
@@ -2,7 +2,7 @@
  * The current version of the library.
  * This version number is updated automatically during the build process.
  * The version number is in the format: Major.Minor.Patch
- * @example 
+ * @example
  * console.log(__VERSION__);
  * // Output 1.0.0
  */
