a001a7bcf46a844ebc45f6490168e4953fdecc9e
Lewis Moten <lewismoten@gmail.com>
2024-03-26 12:21:41 -0400

    :white_check_mark: Ensure package doesn't have dependencies

--- __tests__/package.test.ts
+++ __tests__/package.test.ts
@@ -4,4 +4,7 @@ describe('Package', () => {
   it('has no more than 20 keywords', () => {
     expect(packageJson.keywords.length).toBeLessThanOrEqual(20);
   });
+  it('has no dependencies', () => {
+    expect('dependencies' in packageJson).toBe(false);
+  })
 });
