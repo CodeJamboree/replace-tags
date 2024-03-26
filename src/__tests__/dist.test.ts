56d2a603626651298fdf22f022bbdb0d7dc82982
Lewis Moten <lewismoten@gmail.com>
2024-03-26 12:29:02 -0400

    :fire: build files already tested with `test:examples`
    We mainly want to let the tests run without error if the project hasn't been built before, or that the dist/dist-dev folders do not exist.

--- src/__tests__/dist.test.ts
+++
@@ -1,9 +0,0 @@
-import replaceTags from "../../dist/index.min.js";
-
-describe("dist", () => {
-  it("replaces tag", () => {
-    const text = "Hello {{name}}!";
-    const values = { name: "World" };
-    expect(replaceTags(text, values)).toBe("Hello World!");
-  });
-});
