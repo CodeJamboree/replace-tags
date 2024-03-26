1a23e24525eb3288494115bc4e0c9615825ae1c0
Lewis Moten <lewismoten@gmail.com>
2024-03-26 16:33:25 -0400

    :label: Typescript __VERSION__

--- src/globals.d.ts
+++ src/globals.d.ts
@@ -1,1 +1,9 @@
+/**
+ * The current version of the library.
+ * This version number is updated automatically during the build process.
+ * The version number is in the format: Major.Minor.Patch
+ * @example 
+ * console.log(__VERSION__);
+ * // Output 1.0.0
+ */
 declare const __VERSION__: string;
