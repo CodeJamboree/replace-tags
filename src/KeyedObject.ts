dc9d1f84ee92aea42a43a019cb9b26a3f8cda160
Lewis Moten <lewismoten@gmail.com>
2024-03-26 16:37:57 -0400

    :recycle: Use record over custom interface

--- src/KeyedObject.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Interface for an object with string keys and values of a generic type.
- * @template T - The type of the values. Defaults to `unknown`.
- */
-interface KeyedObject<T = unknown> {
-  [key: string]: T;
-}
-export default KeyedObject;
--- src/getValue.ts
+++ src/getValue.ts
@@ -1,8 +1,6 @@
-import KeyedObject from "./KeyedObject";
-
 /**
- * Retrieves the value from the source object based on the provided key
- * @param {unknown} source The source object from which to retrieve the value.
+ * Retrieves the value from the source based on the provided key
+ * @param {unknown} source The source from which to retrieve the value.
  * @param {string|number} key The key to look up in the source object.
  * @param {string} currentPath The current path being traversed in the object hierarchy.
  * @param {string} fullPath The full path to the current key being evaluated.
@@ -19,7 +17,7 @@ const getValue = (
     if (Array.isArray(source) && /\d+/.test(key)) {
       value = source[parseInt(key)];
     } else {
-      value = (source as KeyedObject)[key];
+      value = (source as Record<string, unknown>)[key];
     }
     if (typeof value === "function")
       value = value.call(source, key, currentPath, fullPath);
