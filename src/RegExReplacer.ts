ebe8b639e610fe16eccc34a93d9fb90313267350
Lewis Moten <lewismoten@gmail.com>
2024-03-26 16:06:22 -0400

    :label: Typescript

--- src/RegExReplacer.ts
+++ src/RegExReplacer.ts
@@ -1,4 +1,18 @@
+/**
+ * The RegExReplacer interface defines a function that takes a string and returns a string.
+ * @param {string} match - The matched tag.
+ * @returns {string} The resolved value if the tag matches a path in the `values` object; otherwise, the original tag.
+ * @example
+ * const replacer: RegExReplacer = (match: string): string => {
+ *  match === "hello" ? "world" : "hello";
+ * };
+ */
 interface RegExReplacer {
+  /**
+   * Function that takes a match string as an argument and returns a replacement string.
+   * @param match - The matched tag.
+   * @returns The resolved value if the tag matches a path in the `values` object; otherwise, the original tag.
+   */
   (match: string): string;
 }
 
