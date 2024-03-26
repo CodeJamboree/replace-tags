376c731456403b10a058c61dfd7223b67d4812da
Lewis Moten <lewismoten@gmail.com>
2024-03-26 14:48:59 -0400

    :bulb: Updating JSDoc

--- src/tagReplacer.ts
+++ src/tagReplacer.ts
@@ -5,7 +5,7 @@ import ReplaceTagsOptions from "./ReplaceTagsOptions";
 /**
  *
  * Creates a RegEx replacer function to use the corresponding values and tag style.
- * @param {object | string} values - The object containing values for replacement.
+ * @param {object} values - The object containing values for replacement.
  * @param {ReplaceTagsOptions} options - configuration for tag parsing.
  * @returns {RegExReplacer} A function that takes a tag and replaces it with a value.
  */
@@ -13,19 +13,21 @@ const tagReplacer =
   (
     values: object,
     { tagStartPattern, tagEndPattern }: ReplaceTagsOptions,
-  ): RegExReplacer =>
+  ): RegExReplacer => {
   /**
-   * Replaces a tag with its resolved value
+   * Replaces a tag with its resolved value if found; otherwise, returns the tag.
    * @param {string} match The matched tag
-   * @returns {string} The replaced value
+   * @returns {string} The resolved value if found; otherwise, the tag.
    */
-  (match: string): string => {
+  const replacer: RegExReplacer = (match: string): string => {
     const path = match
       .replace(tagStartPattern, "")
       .replace(tagEndPattern, "")
       .trim();
     const value = findValueByPath(values, path);
-    return value !== undefined ? `${value}` : match;
+    return value === undefined ? match : `${value}`
   };
 
+  return replacer;
+}
 export default tagReplacer;
