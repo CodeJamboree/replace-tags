2161883f26514b3e52075a183d2a4e19456c20c3
Lewis Moten <lewismoten@gmail.com>
2024-03-26 15:07:53 -0400

    :zap: create regex once

--- src/tagReplacer.ts
+++ src/tagReplacer.ts
@@ -13,21 +13,17 @@ const tagReplacer = (
   values: object,
   { tagStartPattern, tagEndPattern }: ReplaceTagsOptions,
 ): RegExReplacer => {
+  const tagEdges = new RegExp(
+    `${tagStartPattern.source}|${tagEndPattern.source}`,
+    "g",
+  );
   /**
    * Replaces a tag with its resolved value if found; otherwise, returns the tag.
    * @param {string} match The matched tag
-   * @returns {string} The resolved value if found; otherwise, the tag.
+   * @returns {string} The resolved value if the tag matches a path in the `values` object; otherwise, the original tag.
    */
   const replacer: RegExReplacer = (match: string): string => {
-    const path = match
-      .replace(
-        new RegExp(
-          `${tagStartPattern.source}|${tagEndPattern.source}`,
-          "g",
-        ),
-        "",
-      )
-      .trim();
+    const path = match.replace(tagEdges, "").trim();
     const value = findValueByPath(values, path);
     return `${value ?? match}`;
   };
