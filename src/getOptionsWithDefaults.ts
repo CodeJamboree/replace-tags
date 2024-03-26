480a36760c9d0c9caa3b0302cae092b772efc045
Lewis Moten <lewismoten@gmail.com>
2024-03-26 08:06:39 -0400

    :bug:Copy stateful regular expressions

--- src/getOptionsWithDefaults.ts
+++ src/getOptionsWithDefaults.ts
@@ -1,18 +1,37 @@
 import ReplaceTagsOptions from "./ReplaceTagsOptions";
 import defaultTag from "./styles/DoubleCurlyBraces";
 
+interface IgetOptionsWithDefaults {
+  (
+    options: Partial<ReplaceTagsOptions> | undefined,
+  ): ReplaceTagsOptions;
+}
+
 /**
  * Merges the provided options with default options for tag patterns.
  * @param {Partial<ReplaceTagsOptions>} [options] - Optional partial options object.
  * @returns {ReplaceTagsOptions} The merged options object with default values.
  */
-const getOptionsWithDefaults = (
-  options?: Partial<ReplaceTagsOptions>,
-): ReplaceTagsOptions => ({
-  tagPattern: options?.tagPattern ?? defaultTag.tagPattern,
-  tagStartPattern:
-    options?.tagStartPattern ?? defaultTag.tagStartPattern,
-  tagEndPattern: options?.tagEndPattern ?? defaultTag.tagEndPattern,
+const getOptionsWithDefaults: IgetOptionsWithDefaults = ({
+  tagPattern,
+  tagStartPattern,
+  tagEndPattern,
+} = defaultTag): ReplaceTagsOptions => ({
+  tagPattern: getRegExp(tagPattern, defaultTag.tagPattern),
+  tagStartPattern: getRegExp(
+    tagStartPattern,
+    defaultTag.tagStartPattern,
+  ),
+  tagEndPattern: getRegExp(tagEndPattern, defaultTag.tagEndPattern),
 });
+
+const getRegExp = (
+  expression: RegExp | undefined,
+  defaultExpression: RegExp,
+): RegExp => {
+  const value = expression ?? defaultExpression;
+  // return copy of expressions with state
+  return value.lastIndex !== 0 ? new RegExp(value) : value;
+};
 
 export default getOptionsWithDefaults;
