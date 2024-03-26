45fa73006130874c5702ff8c28fa254bf330d30f
Lewis Moten <lewismoten@gmail.com>
2024-03-26 16:12:43 -0400

    :label: JSDoc KeyedObject

--- src/KeyedObject.ts
+++ src/KeyedObject.ts
@@ -1,3 +1,7 @@
+/**
+ * Interface for an object with string keys and values of a generic type.
+ * @template T - The type of the values. Defaults to `unknown`.
+ */
 interface KeyedObject<T = unknown> {
   [key: string]: T;
 }
