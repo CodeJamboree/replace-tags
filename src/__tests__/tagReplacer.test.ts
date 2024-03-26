9b5ce8c461e6dba17b98ba84e8da727a8ba5214f
Lewis Moten <lewismoten@gmail.com>
2024-03-26 17:16:34 -0400

    :art: pretty

--- src/__tests__/tagReplacer.test.ts
+++ src/__tests__/tagReplacer.test.ts
@@ -1,5 +1,5 @@
 import DoubleCurlyBraces from "../styles/DoubleCurlyBraces";
-import tagReplacer from "../tagReplacer"
+import tagReplacer from "../tagReplacer";
 
 describe("tagReplacer", () => {
   const tagEdges = /^{{\s*|\s*}}$/g;
@@ -8,7 +8,12 @@ describe("tagReplacer", () => {
       name: "John Doe",
     };
     const replacer = tagReplacer(values, tagEdges);
-    expect("Name: {{name}}".replace(DoubleCurlyBraces.tagPattern, replacer)).toBe("Name: John Doe");
+    expect(
+      "Name: {{name}}".replace(
+        DoubleCurlyBraces.tagPattern,
+        replacer,
+      ),
+    ).toBe("Name: John Doe");
   });
 
   it("should handle missing values gracefully", () => {
@@ -16,17 +21,22 @@ describe("tagReplacer", () => {
       name: "John Doe",
     };
     const replacer = tagReplacer(values, tagEdges);
-    expect("Age: {{age}}".replace(DoubleCurlyBraces.tagPattern, replacer)).toBe("Age: {{age}}");
+    expect(
+      "Age: {{age}}".replace(DoubleCurlyBraces.tagPattern, replacer),
+    ).toBe("Age: {{age}}");
   });
   it("should cache tag results", () => {
     const mockName = jest.fn().mockReturnValue("John Doe");
     const values = {
-      name: mockName
+      name: mockName,
     };
     const replacer = tagReplacer(values, tagEdges);
-    expect("Name 1: {{name}}; Name 2: {{ name }}".replace(DoubleCurlyBraces.tagPattern, replacer)).toBe(
-      "Name 1: John Doe; Name 2: John Doe");
+    expect(
+      "Name 1: {{name}}; Name 2: {{ name }}".replace(
+        DoubleCurlyBraces.tagPattern,
+        replacer,
+      ),
+    ).toBe("Name 1: John Doe; Name 2: John Doe");
     expect(mockName).toHaveBeenCalledTimes(1);
   });
 });
-
