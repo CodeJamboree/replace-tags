9a33be522e3d7bb2972b25f38096963286f37074
Lewis Moten <lewismoten@gmail.com>
2024-03-26 14:43:10 -0400

    :bulb: Add examples for ReplaceTagsOptions

--- src/ReplaceTagsOptions.ts
+++ src/ReplaceTagsOptions.ts
@@ -5,17 +5,20 @@
 interface ReplaceTagsOptions {
   /**
    * The regular expression pattern for matching tags.
+   * @example /{{\s*?.*?\s*}}/g
    * @type {RegExp}
    */
   tagPattern: RegExp;
   /**
    * The regular expression pattern for matching the start of a tag.
+   * @example /{{\s*?/g.
    * @type {RegExp}
    */
   tagStartPattern: RegExp;
 
   /**
    * The regular expression pattern for matching the end of a tag.
+   * @example /\s*}}/g.
    * @type {RegExp}
    */
   tagEndPattern: RegExp;
