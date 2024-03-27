1f868646fbfa3656a6b8efe9672611394931d5b7
Lewis Moten <lewismoten@gmail.com>
2024-03-26 22:11:35 -0400

    :label: Update JSDoc

--- src/checkCondition.ts
+++ src/checkCondition.ts
@@ -3,8 +3,11 @@
  * @param {boolean} condition - The condition to check.
  * @param {string} message - The error message to throw if the condition is met.
  * @throws {Error} Throws an error if the condition is met.
+ * @returns {void} Nothing.
+ * @example checkCondition(1 === 1, "1 is equal to 1");
+ * // Throws an error with the message "1 is equal to 1"
  */
-const checkCondition = (condition: boolean, message: string) => {
+const checkCondition = (condition: boolean, message: string): void => {
   if (condition) {
     throw new Error(message);
   }
