0cb1a8c36b8405d32bb0e3417efb5cf5834c0f8c
Lewis Moten <lewismoten@gmail.com>
2024-03-26 21:51:01 -0400

    :art: pretty

--- src/getRegExp.ts
+++ src/getRegExp.ts
@@ -4,11 +4,11 @@
  * @param {RegExp} [expression] An regular expression, or undefined
  * @returns {RegExp} A stateless regular expression
  */
-const getRegExp = (
-  expression: RegExp,
-): RegExp => {
+const getRegExp = (expression: RegExp): RegExp => {
   // Return a new RegExp if lastIndex is not 0 to ensure the returned RegExp is stateless
-  return expression.lastIndex !== 0 ? new RegExp(expression) : expression;
+  return expression.lastIndex !== 0
+    ? new RegExp(expression)
+    : expression;
 };
 
 export default getRegExp;
