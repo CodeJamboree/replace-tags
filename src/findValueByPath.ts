cb7854b5fedaa19624edf2a9a2c52b7592c783e1
Lewis Moten <lewismoten@gmail.com>
2024-03-26 21:57:19 -0400

    :recycle: use for loop instead of reducer

--- src/findValueByPath.ts
+++ src/findValueByPath.ts
@@ -2,16 +2,22 @@ import findNextValue from "./findNextValue";
 import NextValue from "./NextValue";
 
 /**
- * Finds a value in an object by the given path.
- * @param {object} source - The object to search within.
+ * Finds a value in an object by the given path in dot notation.
+ * @param {unknown} source - The object to search within.
  * @param {string} path - The path to the value in dot notation.
  * @returns {unknown} The value found at the given path, or undefined if not found.
  */
-const findValueByPath = (source: object, path: string): unknown =>
-  path.split(".").reduce<NextValue>(findNextValue, {
-    fullPath: path,
-    currentPath: undefined,
-    value: source,
-  }).value;
-
+const findValueByPath = (source: unknown, path: string): unknown => {
+  const segments = path.split(".");
+  let nextValue: NextValue = {
+      fullPath: path,
+      currentPath: undefined,
+      value: source,
+    }
+  for(const segment of segments) {
+    nextValue = findNextValue(nextValue, segment);
+    if (nextValue.value === undefined) return;
+  }
+  return nextValue.value;
+}
 export default findValueByPath;
